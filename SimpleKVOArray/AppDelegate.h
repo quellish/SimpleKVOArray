794c0ea42211aad35b9f9f3cd2537174662bf8bd
quellish <github@quellish.neomailbox.com>
2014-08-30 22:23:08 -0700

    Initial Commit

---
+++ SimpleKVOArray/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleKVOArray
+//
+//  Created by Dan Zinngrabe on 8/30/14.
+//  Copyright (c) 2014 Dan Zinngrabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
